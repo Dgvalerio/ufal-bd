b573368d8f8a4916bea5106c500f63a91a7a7482
Davi <al.davigvalerio@gmail.com>
2022-05-24 23:21:09 -0300

    feat: add questions

---
+++ 02.brinks.sql
@@ -0,0 +1,25 @@
+# DROP DATABASE `UFAL_MYSQL`;
+use UFAL_MYSQL;
+
+# 1. Nomes dos clientes da locadora
+
+
+# 2. Nome dos filmes que já foram locados pela cliente ‘Juliana Santos’
+
+
+# 3. Nome dos filmes que possuem unidades com legenda em ‘Português’ ou ‘Inglês’
+
+
+# 4. O nome e o código dos filmes que nunca foram alugados.
+
+
+# 5. O nome dos clientes que já alugaram filme com legenda em ‘Inglês’.
+
+
+# 6. O nome do filme e o número de unidades que existem na locadora para cada filme.
+
+
+# 7. O nome do(s) cliente(s) que locou mais vezes o filme ‘AXL’
+
+
+# 8. Nome dos filmes que possuem unidades com áudio em ‘Português’ e ‘Inglês’
